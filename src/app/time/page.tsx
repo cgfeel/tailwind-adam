a839c959e989518216e68071a10e3711d70a2219
levi <cgfeel@gmail.com>
2024-11-25 22:26:21 +0800

    update

--- src/app/time/page.tsx
+++ src/app/time/page.tsx
@@ -9,6 +9,7 @@ export default async function Page() {
                 <li>静态路由不包含 `[]`，路由下的 `fetch` 将在构建时缓存数据，除非定时或手动更新</li>
                 <li>动态路由以 `[]` 包裹，路由下的 `fetch` 将实时更新</li>
             </ul>
+            <div className="p-4">`dev` 环境下 `ssr` 每次都会刷新数据</div>
             <h3 className="my-4 text-xl font-bold">layout.tsx 和 template.tsx：</h3>
             <ul className="list-inside list-disc">
                 <li>layout.tsx：布局下 `client component` 的 `hook` 状态将会被保存，不随路由切换清空</li>
